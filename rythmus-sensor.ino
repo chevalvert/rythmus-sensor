3cc5d1a05c2f7c006a0ee64d0836ec00394f6654
Arnaud Juracek <arnaud.juracek@gmail.com>
2023-11-26 22:41:59 +0100

    Remove capacitive sensor

--- rythmus-sensor.ino
+++ rythmus-sensor.ino
@@ -1,21 +1,26 @@
-#define USE_ARDUINO_INTERRUPTS false
+#define USE_ARDUINO_INTERRUPTS true    
 #include <PulseSensorPlayground.h>
 
 const int PULSESENSOR_PIN = 0;
-const int CAPACITIVE_PIN = 5;
-const int LED_PIN = 2;
+const int SWITCH_PIN = 9;
+const int LED_SWITCH_PIN = 3;
+const int LED_PULSE_PIN = 4;
+
 const byte SAMPLES_PER_SERIAL_SAMPLE = 10;
 
 int incomingByte;
-int capacitive_cycles = 0;
 byte samplesUntilReport;
 PulseSensorPlayground pulseSensor;
 
 void setup () {
   Serial.begin(115200);
 
-  pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, LOW);
+  pinMode(LED_PULSE_PIN, OUTPUT);
+  pinMode(LED_SWITCH_PIN, OUTPUT);
+  pinMode(SWITCH_PIN, INPUT);
+
+  digitalWrite(LED_PULSE_PIN, LOW);
+  digitalWrite(LED_SWITCH_PIN, LOW);
 
   pulseSensor.analogInput(PULSESENSOR_PIN);
   pulseSensor.setSerial(Serial);
@@ -23,33 +28,57 @@ void setup () {
 
   // NOTE: feedback if something goes wrong
   if (!pulseSensor.begin()) failureFeedback();
+  else okFeedback();
 }
 
 void loop () {
+  // Get switch state and display its state
+  const int pressed = digitalRead(SWITCH_PIN);
+  digitalWrite(LED_SWITCH_PIN, pressed);
+
+  // Handle pulse led
   if (Serial.available() > 0) {
     incomingByte = Serial.read();
-    if (incomingByte == 'H') digitalWrite(LED_PIN, HIGH);
-    if (incomingByte == 'L') digitalWrite(LED_PIN, LOW);
+    if (incomingByte == 'H') digitalWrite(LED_PULSE_PIN, HIGH);
+    if (incomingByte == 'L') digitalWrite(LED_PULSE_PIN, LOW);
   }
 
+  // Read pulse sensor
   if (pulseSensor.sawNewSample()) {
     if (--samplesUntilReport == (byte) 0) {
       samplesUntilReport = SAMPLES_PER_SERIAL_SAMPLE;
-
-      Serial.print(capacitive_cycles);
+      
+      // Send sensor states via Serial
+      // Sending 100|0 to simulate capacitive cycles used in previous version of this sensor for retrocompatibility
+      if (pressed == HIGH) Serial.print(100);
+      else Serial.print(0);
       Serial.write(',');
       pulseSensor.outputSample();
     }
-
-    capacitive_cycles = readCapacitivePin(CAPACITIVE_PIN);
   }
+}
+
+void okFeedback () {
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(LED_PULSE_PIN, LOW);
+    digitalWrite(LED_SWITCH_PIN, HIGH);
+    delay(200);
+    digitalWrite(LED_PULSE_PIN, HIGH);
+    digitalWrite(LED_SWITCH_PIN, LOW);
+    delay(200);
+  }
+
+  digitalWrite(LED_PULSE_PIN, LOW);
+  digitalWrite(LED_SWITCH_PIN, LOW);
 }
 
 void failureFeedback () {
   for(;;) {
-    digitalWrite(LED_PIN, LOW);
+    digitalWrite(LED_PULSE_PIN, LOW);
+    digitalWrite(LED_SWITCH_PIN, LOW);
     delay(200);
-    digitalWrite(LED_PIN, HIGH);
+    digitalWrite(LED_PULSE_PIN, HIGH);
+    digitalWrite(LED_SWITCH_PIN, HIGH);
     delay(200);
   }
 }
